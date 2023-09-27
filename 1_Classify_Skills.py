3901785c400c22470768efdb224a6e91db914c16
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2023-09-27 13:08:33 -0600

    Se limpia input al dar enter

--- 1_Classify_Skills.py
+++ 1_Classify_Skills.py
@@ -5,8 +5,14 @@ from scripts import classify_interface as ci
 def iterator_session(iterator:iter):
     st.session_state.iterator = iterator
 
+def submit():
+    st.session_state.something = st.session_state.widget
+    st.session_state.widget = ''
 
 def render_page():
+    if 'something' not in st.session_state:
+        st.session_state.something = ''
+
     st.title('Labeling')
     classifier = ci.LabelingInterface('/data/skills_dataset.parquet', '/skills_classified.parquet', current_dir='.')
     unclassified_skills = classifier.get_unclassified_skills()
@@ -16,7 +22,7 @@ def render_page():
     
     st.write(job)
     st.write(skill)
-    label = st.text_input('Give the label')
+    label = st.text_input('Give the label', key='widget', on_change=submit)
     
     new_label = [list(row.values) + [label]]
     classifier.save_new_classifications(new_label)
